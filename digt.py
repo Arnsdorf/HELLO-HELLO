552791d9df1ed6fbced4b58a73f2161d02455ee2
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2019-08-19 10:50:45 +0200

    Fixet syntax

--- digt.py
+++ digt.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
 print("Forestillingerne lukkes snart i samme hovede, ørerne overhovedet,Udstrakte flader på mig, gul virkelighed, tænder, engel, uafbrudt , i usynlige\nfiktioner som glammer på os, hånd bor sofa, når\ndet følger mere end ellers, kroppene og sadlen og det historisk modsatte herskab vænner vi os over, skønt vi leder kroppen, selv fortovet er nødvendigt endnu og undervejs glinsende, allerede bliver det måske vegetation.")
-=======
+
 print("Maskeballet lukkes snart i samme hovede, ørerne overhovedet, i usynlige\n fiktioner som glammer på os, hånd bor installationskunst, når det følger mere end ellers, kroppene og sadlen og det historisk ukorrekte herskab vænner vi os over, skønt vi leder kroppen, den vænner sig tilbage sig selv, selv fortovet er ligegyldigt endnu og undervejs glinsende, allerede bliver det måske vegetation.")
->>>>>>> 756ec5927f0a897c12d47583d3f9d2fa85d3ff62
+
