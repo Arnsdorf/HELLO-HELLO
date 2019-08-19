a3cd4da4bf47e1d942d7236aeffd0ad5402abe92
Agger0897 <38247158+Agger0897@users.noreply.github.com>
2019-08-19 10:52:40 +0200

    Tilføjet en print funktion som der er blevet ændret lidt på teksten

--- digt.py
+++ digt.py
@@ -2,3 +2,4 @@ print("Forestillingerne lukkes snart i samme hovede, ørerne overhovedet,Udstrak
 
 print("Maskeballet lukkes snart i samme hovede, ørerne overhovedet, i usynlige\n fiktioner som glammer på os, hånd bor installationskunst, når det følger mere end ellers, kroppene og sadlen og det historisk ukorrekte herskab vænner vi os over, skønt vi leder kroppen, den vænner sig tilbage sig selv, selv fortovet er ligegyldigt endnu og undervejs glinsende, allerede bliver det måske vegetation.")
 
+print("fiktioner som glammer på os\n Maskeballet lukkes snart i samme hovede, ørerne overhovedet, i usynlige, hånd bor installationskunst, når det følger mere end ellers, kroppene og sadlen og det historisk ukorrekte herskab vænner vi os over, skønt vi leder kroppen, den vænner sig tilbage sig selv, selv fortovet er ligegyldigt endnu og undervejs glinsende, allerede bliver det måske vegetation.")
